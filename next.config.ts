7b55a61769731306424db6dc4e020558630790a9
Genesis Jr <72869897+Sliceofbanana@users.noreply.github.com>
2025-08-11 13:53:45 +0800

    081125 - lain nasda haysss

--- next.config.ts
+++ next.config.ts
@@ -26,10 +26,9 @@ const nextConfig: NextConfig = {
               value: 'Content-Type, Authorization',
             },
           ] : []),
-          // Security headers
           {
             key: 'X-Frame-Options',
-            value: 'DENY',
+            value: 'SAMEORIGIN',
           },
           {
             key: 'X-Content-Type-Options',
@@ -40,18 +39,20 @@ const nextConfig: NextConfig = {
             value: 'strict-origin-when-cross-origin',
           },
           {
-          key: 'Content-Security-Policy',
-          value: [
-            "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://ssl.gstatic.com https://content.googleapis.com",
-            "style-src 'self' 'unsafe-inline' https://accounts.google.com https://www.gstatic.com",
-            "img-src 'self' data: https: blob:",
-            "connect-src 'self' https://*.googleapis.com https://*.firebaseapp.com https://identitytoolkit.googleapis.com wss://*.firebaseapp.com https://accounts.google.com https://www.googleapis.com https://oauth2.googleapis.com https://apis.google.com https://content.googleapis.com https://*.firebaseio.com https://*.cloudfunctions.net",
-            "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://content.googleapis.com https://apis.google.com https://*.gstatic.com https://accounts.youtube.com",
-            "object-src 'none'",
-            "base-uri 'self'",
-            "form-action 'self'"
-          ].join('; ')
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://ssl.gstatic.com https://content.googleapis.com",
+              "style-src 'self' 'unsafe-inline' https://accounts.google.com https://www.gstatic.com https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: https: blob: https://www.gstatic.com https://ssl.gstatic.com",
+              "connect-src 'self' https://*.googleapis.com https://*.firebaseapp.com https://identitytoolkit.googleapis.com wss://*.firebaseapp.com https://accounts.google.com https://www.googleapis.com https://oauth2.googleapis.com https://apis.google.com https://content.googleapis.com https://*.firebaseio.com https://*.cloudfunctions.net https://www.gstatic.com https://ssl.gstatic.com",
+              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://content.googleapis.com https://apis.google.com https://*.gstatic.com https://ssl.gstatic.com https://accounts.youtube.com https://*.google.com",
+              "object-src 'none'",
+              "base-uri 'self'",
+              "form-action 'self' https://accounts.google.com",
+              "frame-ancestors 'none'"
+            ].join('; ')
           },
         ],
       },
