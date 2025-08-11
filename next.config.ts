5155ac9c8740aef91a7de525a31ec4038911d7f4
Genesis Jr <72869897+Sliceofbanana@users.noreply.github.com>
2025-08-11 13:41:30 +0800

    081125 - lain nasab

--- next.config.ts
+++ next.config.ts
@@ -43,14 +43,14 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://accounts.google.com https://ssl.gstatic.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com",
-              "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: https: https://ssl.gstatic.com https://www.gstatic.com",
-              "connect-src 'self' https://*.googleapis.com https://*.firebaseapp.com https://identitytoolkit.googleapis.com wss://*.firebaseapp.com https://accounts.google.com https://www.googleapis.com https://oauth2.googleapis.com",
-              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://job-tracker-169f2.firebaseapp.com https://content.googleapis.com https://accounts.google.com/gsi/",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com",
+              "style-src 'self' 'unsafe-inline' https://accounts.google.com https://www.gstatic.com",
+              "img-src 'self' data: https: blob:",
+              "connect-src 'self' https://*.googleapis.com https://*.firebaseapp.com https://identitytoolkit.googleapis.com wss://*.firebaseapp.com https://accounts.google.com https://www.googleapis.com https://oauth2.googleapis.com https://apis.google.com https://content.googleapis.com https://*.firebaseio.com https://*.cloudfunctions.net",
+              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://content.googleapis.com https://apis.google.com",
               "object-src 'none'",
-              "frame-ancestors 'none'"
+              "base-uri 'self'",
+              "form-action 'self'"
             ].join('; ')
           },
         ],
